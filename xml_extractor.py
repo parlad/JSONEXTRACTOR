c7162483b748e549ab9f4f03387977040d16b4d1
Pralad Neupane <parladneupane@gmail.com>
2023-11-07 09:24:37 -0500

    Update xml_extractor.py
    added json structre option

--- xml_extractor.py
+++ xml_extractor.py
@@ -1,35 +1,43 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
+import json
 
 # Read the mapping file
 mapping_df = pd.read_csv('mapping.csv')
 
+# Read the JSON data from a file
+with open('input2.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
 # Initialize an empty list to store the data
 data = []
 
-# Parse the XML file
-tree = ET.parse('sample.xml')
-root = tree.getroot()
+for item in json_data:
+    # Parse the XML string from the JSON data
+    xml_string = item['consolidatereportdetailtext']
+    root = ET.fromstring(xml_string)
 
-# Iterate through root elements in the XML
-for element in root:
-    # Initialize a dictionary to store the data for this root element
-    root_data = {}
+    # Iterate through root elements in the XML
+    for element in root:
+        # Initialize a dictionary to store the data for this XML
+        xml_data = {}
 
-    # Iterate through the mapping DataFrame
-    for index, row in mapping_df.iterrows():
-        xml_path = row['xml_path']
-        column_name = row['column_name']
+        # Add "_id" value to the dictionary
+        xml_data["_id"] = item["_id"]["$oid"]
 
-        # Find the element in the current root using the path
-        elements = element.findall(xml_path)
+        # Iterate through the mapping DataFrame
+        for index, row in mapping_df.iterrows():
+            xml_path = row['xml_path']
+            column_name = row['column_name']
 
-        # Extract the values and append to the dictionary
-        values = [element.text if element is not None else '' for element in elements]
-        root_data[column_name] = values
+            # Find the element in the current root using the path
+            elements = element.findall(xml_path)
 
-    # Append the data for this root element to the list
-    data.append(root_data)
+            # Extract the values and append to the dictionary
+            values = [element.text if element is not None else '' for element in elements]
+            xml_data[column_name] = values
+
+        data.append(xml_data)
 
 # Create a DataFrame from the extracted data
 result_df = pd.DataFrame(data)
@@ -44,5 +52,5 @@ result_df.reset_index(drop=True, inplace=True)
 # Replace empty strings with None
 result_df = result_df.applymap(lambda x: None if x == '' else x)
 
-# Export the DataFrame as a CSV file
-result_df.to_csv('output.csv', index=False)
+# Show the resulting DataFrame
+print(result_df)
